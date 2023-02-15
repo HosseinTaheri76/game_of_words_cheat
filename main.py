ab8af16f857fe047fa232240390c42ca6a3639d2
HosseinTaheri76 <36724808+HosseinTaheri76@users.noreply.github.com>
2023-02-15 14:06:46 +0330

    initiate functionality for combining alphabets

---
+++ main.py
@@ -0,0 +1,9 @@
+import itertools
+
+def get_alphabets():
+    alphabets = []
+
+scope = ['آ', 'س', 'ا', 'ب', 'ی', 'غ', 'ژ', 'ر']
+length = len(scope)
+chain = itertools.chain.from_iterable((itertools.permutations(scope, r=i) for i in range(3, 7)))
+map_chain = map(lambda x: ''.join(x), chain)
